618e3dd8ab88c7cb3f882bd16bcf44a894de5f9a
Shivani Mali <shivanimali339@gmail.com>
2024-09-02 00:14:08 +0530

    !st file

---
+++ numberpri.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class numberpri {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter some value:");
+        int n=sc.nextInt();
+        System.out.println("Value="+n);
+        sc.close();
+    }
+}
---
+++ rectangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class rectangle {
+    public static void main(String[] args) {
+        int l,b,area;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Emter the value of l and b");
+        l =sc.nextInt();
+        b =sc.nextInt();
+        area = l*b;
+        System.out.println("Area of rectangle is:"+area);
+        
+
+    }
+}
